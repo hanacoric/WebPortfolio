2a5656e82c889f57261c6d387e65e621cec4ce79
hanacoric <hancor01@easv365.dk>
2023-04-22 00:29:58 +0200

    Made mobile version

--- PORTFOLIO PROJECT WEB/main.js
+++ PORTFOLIO PROJECT WEB/main.js
@@ -23,7 +23,7 @@ function isInView(element) {
   window.addEventListener('scroll', onScroll);
 
   const slideshowPosters = document.querySelector('.slideshow_posters');
-      const imagesPosters = ['/resources/images/child labour poster.jpg', '/resources/images/longhornbbq-poster_1 (2).jpg'];
+      const imagesPosters = ['../PORTFOLIO PROJECT WEB/resources/images/child labour poster.jpg', '../PORTFOLIO PROJECT WEB/resources/images/longhornbbq-poster_1 (2).jpg'];
       let currentIndex = 0;
 
       setInterval(() => {
@@ -32,7 +32,7 @@ function isInView(element) {
       }, 2000);
 
       const slideshowLogos = document.querySelector('.slideshow_logos');
-      const imagesLogos = ['/resources/images/child labour logo.png', '/resources/images/longhornbbqlogojpg.jpg'];
+      const imagesLogos = ['../PORTFOLIO PROJECT WEB/resources/images/child labour logo.png', '../PORTFOLIO PROJECT WEB/resources/images/longhornbbqlogojpg.jpg'];
       let currentIndexLogos = 0;
 
       setInterval(() => {
@@ -43,9 +43,9 @@ function isInView(element) {
       document.getElementById("download-pdf-btn").href = "/resources/cv/portfolio-cv.pdf";
 
       const slideshowWebsite = document.querySelector('.website_img');
-      const imagesWebsite = ['/resources/images/website2.png', '/resources/images/website3.png' ,
-     '/resources/images/website4.png', '/resources/images/website5.png', '/resources/images/website6.png', 
-      '/resources/images/website.png'];
+      const imagesWebsite = ['../PORTFOLIO PROJECT WEB/resources/images/website2.png', '../PORTFOLIO PROJECT WEB/resources/images/website3.png' ,
+     '../PORTFOLIO PROJECT WEB/resources/images/website4.png', '../PORTFOLIO PROJECT WEB/resources/images/website5.png', '../PORTFOLIO PROJECT WEB/resources/images/website6.png', 
+      '../PORTFOLIO PROJECT WEB/resources/images/website.png'];
       let currentIndexWebsite = 0;
 
       setInterval(() => {
